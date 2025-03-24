d208229019903d2f51c8235e611440b833c722b0
jmdsgithub <josemendezds@gmail.com>
2025-03-24 20:24:34 +0100

    Add files via upload

---
+++ Oracle XE.sql
@@ -0,0 +1,15 @@
+--comentarios
+
+/*
+comentarios mas de una línea
+*/
+
+--no diferencia mayúsculas de minúsculas en las palabras clave
+
+select * from EMP;
+select * from DEPT;
+
+select DEPT_NO, DNOMBRE, LOC from DEPT;
+
+
+
